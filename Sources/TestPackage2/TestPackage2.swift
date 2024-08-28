13a9cfaeed70b524aa34b8c45697cde0c5c329e9
Albert <alhafram@gmail.com>
2024-08-28 09:51:36 +0300

    Initial commit

---
+++ Sources/TestPackage2/TestPackage2.swift
@@ -0,0 +1,21 @@
+open class TestPackage2OpenClass {
+    public init() { }
+}
+
+public class TestPackage2PublicClass {
+    public init() { }
+}
+
+public struct TestPackage2Struct {
+    public init() { }
+}
+
+public enum TestPackage2Enum {
+    case testCase
+}
+
+public actor TestPackage2Actor {
+    public init() { }
+}
+
+public protocol TestPackage2Protocol { }
