f9f5a738c6c3942b32540525bc5fb6644207fa9c
Albert <alhafram@gmail.com>
2024-08-28 10:06:05 +0300

    Fix

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TestPackage2",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS("14.2")],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
